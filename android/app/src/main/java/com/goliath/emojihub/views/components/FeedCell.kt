d64c39f6d062329c3cbd2eb870a78f3985cc5aea
peng-u-0807 <penguin0807@snu.ac.kr>
2023-10-22 01:54:37 +0900

    feat: implement dummy post model

---
+++ android/app/src/main/java/com/goliath/emojihub/views/components/FeedCell.kt
@@ -0,0 +1,16 @@
+package com.goliath.emojihub.views.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun FeedCell(
+
+) {
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FeedCellPreview() {
+    FeedCell()
+}
