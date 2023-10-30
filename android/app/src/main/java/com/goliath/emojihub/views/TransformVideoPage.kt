9b9cd1aa7e0e235f0c1559b67344a13497de9ae4
Dawit Fikru Amanu <bfiqiruu@gmail.com>
2023-10-30 14:08:10 +0900

    implement video transformation upon '변환' button click

--- android/app/src/main/java/com/goliath/emojihub/views/TransformVideoPage.kt
+++ android/app/src/main/java/com/goliath/emojihub/views/TransformVideoPage.kt
@@ -1,5 +1,8 @@
 package com.goliath.emojihub.views
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
@@ -8,26 +11,34 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import com.goliath.emojihub.LocalNavController
+import com.goliath.emojihub.usecases.EmojiUseCaseImpl_Factory
 import com.goliath.emojihub.viewmodels.EmojiViewModel
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TransformVideoPage(
-    viewModel: EmojiViewModel
+    viewModel: EmojiViewModel,
 ) {
     val context = LocalContext.current
     val navController = LocalNavController.current
@@ -39,6 +50,9 @@ fun TransformVideoPage(
             prepare()
         }
     }
+
+    var transforming by remember { mutableStateOf(false) }
+    var resultEmoji by remember { mutableStateOf<Pair<String, String>?>(null) }
 
     Scaffold(
         topBar = {
@@ -58,23 +72,59 @@ fun TransformVideoPage(
                     TextButton(
                         onClick = {
                             // TODO: implement
-                        }
+                            // show progress bar
+                            transforming = true
+
+                            resultEmoji = EmojiUseCase.createEmoji(viewModel.videoUri)
+                            if (resultEmoji != null) transforming = false
+                        },
+                        enabled = !transforming
                     ) {
-                        Text(text = "변환", color = Color.Black)
+                        Text(text = if (resultEmoji != null) "업러드" else "변환", color = Color.Black)
                     }
                 }
             )
         }
     ) { it ->
-        AndroidView(
-            factory = {
-                PlayerView(it).apply {
-                    player = exoPlayer
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            AndroidView(
+                factory = {
+                    PlayerView(it).apply {
+                        player = exoPlayer
+                    }
+                },
+                modifier = Modifier
+                    .padding(it)
+                    .fillMaxSize()
+            )
+
+            if (transforming) {
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center),
+                    color = Color.White
+                )
+            }
+
+            if (resultEmoji != null) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text (
+                        text = String(Character.toChars(resultEmoji!!.first.substring(2).toInt(16))),
+                        fontSize = 48.sp
+                    )
+                    Text (
+                        text = "완료되었습니다",
+                        fontSize = 24.sp,
+                        color = Color.White
+                    )
                 }
-            },
-            modifier = Modifier
-                .padding(it)
-                .fillMaxSize()
-        )
+            }
+        }
+
     }
 }
