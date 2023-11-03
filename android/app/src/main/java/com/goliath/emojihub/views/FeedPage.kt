52fe000bb76f1a734eac3a234c7388244c5b228e
peng-u-0807 <penguin0807@snu.ac.kr>
2023-11-03 18:34:44 +0900

    feat: add `TopNavigationBar`

--- android/app/src/main/java/com/goliath/emojihub/views/FeedPage.kt
+++ android/app/src/main/java/com/goliath/emojihub/views/FeedPage.kt
@@ -1,9 +1,15 @@
 package com.goliath.emojihub.views
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,19 +19,40 @@ import com.goliath.emojihub.models.Post
 import com.goliath.emojihub.models.dummyPost
 import com.goliath.emojihub.ui.theme.Color.EmojiHubDividerColor
 import com.goliath.emojihub.views.components.PostCell
+import com.goliath.emojihub.views.components.TopNavigationBar
 
 @Composable
 fun FeedPage(
     postList: List<Post>
 ) {
-    LazyColumn(
-        modifier = Modifier
-            .fillMaxWidth(),
-        contentPadding = PaddingValues(horizontal = 16.dp)
+    Column (
+
     ) {
-        items(postList) { post ->
-            PostCell(post = post)
-            Divider(color = EmojiHubDividerColor, thickness = 0.5.dp)
+        TopNavigationBar("Feed", shouldNavigate = false) {
+            IconButton(onClick = {
+                print("something")
+            }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = ""
+                )
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth()
+        ) {
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(horizontal = 16.dp)
+            ) {
+                items(postList) { post ->
+                    PostCell(post = post)
+                    Divider(color = EmojiHubDividerColor, thickness = 0.5.dp)
+                }
+            }
         }
     }
 }
--- android/app/src/main/java/com/goliath/emojihub/views/components/TopNavigationBar.kt
+++ android/app/src/main/java/com/goliath/emojihub/views/components/TopNavigationBar.kt
@@ -1,19 +1,23 @@
 package com.goliath.emojihub.views.components
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -21,30 +25,40 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun TopNavigationBar(
     title: String = "",
+    shouldNavigate: Boolean = true,
     navigate: () -> Unit = {},
-    actions: @Composable RowScope.() -> Unit = {},
+    actions: @Composable () -> Unit = {},
 ) {
-    Row(
-        modifier = Modifier.fillMaxSize().padding(horizontal = 4.dp),
-        verticalAlignment = Alignment.CenterVertically,
+    Surface(
+        shape = RectangleShape,
+        elevation = 1.dp,
+        modifier = Modifier
+            .height(64.dp),
     ) {
-        if (title.isNotEmpty()) {
-            IconButton(onClick = { navigate() }) {
-                Icon(
-                    imageVector = Icons.Filled.ArrowBack,
-                    contentDescription = ""
-                )
+        Box(Modifier.padding(horizontal = 4.dp)) {
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                if (shouldNavigate) {
+                    IconButton(onClick = { navigate() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = ""
+                        )
+                    }
+                }
+
+                Text(
+                    text = title,
+                    fontSize = 17.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(start = 12.dp))
+
+                Spacer(modifier = Modifier.weight(1f))
+
+                actions()
             }
         }
-
-        Text(
-            text = title,
-            fontSize = 17.sp,
-            fontWeight = FontWeight.SemiBold,
-            modifier = Modifier.padding(start = 12.dp))
-
-        Spacer(modifier = Modifier.fillMaxWidth(1f))
-
-        actions()
     }
 }
