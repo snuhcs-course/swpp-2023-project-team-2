6289b0f98d8c5730c4ac77fc01ce41888a397525
peng-u-0807 <penguin0807@snu.ac.kr>
2023-11-04 18:39:34 +0900

    fix: remove usage of `CircularProgressIndicator`

--- android/app/src/main/java/com/goliath/emojihub/viewmodels/EmojiViewModel.kt
+++ android/app/src/main/java/com/goliath/emojihub/viewmodels/EmojiViewModel.kt
@@ -3,33 +3,16 @@ package com.goliath.emojihub.viewmodels
 import android.net.Uri
 import androidx.lifecycle.ViewModel
 import com.goliath.emojihub.usecases.EmojiUseCase
-import com.goliath.emojihub.views.PageItem
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
 class EmojiViewModel @Inject constructor(
     private val emojiUseCase: EmojiUseCase
 ): ViewModel() {
-    private val _emojiState = MutableStateFlow<PageItem.Emoji?>(null)
-    val emojiState = _emojiState.asStateFlow()
-
     var videoUri: Uri = Uri.EMPTY
-
-    val loading = MutableStateFlow(false)
 
     fun createEmoji(videoUri: Uri): Pair<String, String>? {
         return emojiUseCase.createEmoji(videoUri)
-    }
-
-    fun updateStateTrue() {
-        loading.update { true }
-    }
-
-    fun updateStateFalse() {
-        loading.update { false }
     }
 }
