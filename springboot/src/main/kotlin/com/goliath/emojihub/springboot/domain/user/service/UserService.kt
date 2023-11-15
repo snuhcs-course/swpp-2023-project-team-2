418d7e70cfee41e43b3e17dad1cab8e05349b98d
Chanhyuk Yang <yangchanhk98@snu.ac.kr>
2023-11-15 22:21:55 +0900

    refactor: refactor code of UserService.signUp&login parameter

--- springboot/src/main/kotlin/com/goliath/emojihub/springboot/domain/user/service/UserService.kt
+++ springboot/src/main/kotlin/com/goliath/emojihub/springboot/domain/user/service/UserService.kt
@@ -6,58 +6,53 @@ import com.goliath.emojihub.springboot.global.exception.CustomHttp401
 import com.goliath.emojihub.springboot.global.exception.CustomHttp404
 import com.goliath.emojihub.springboot.global.exception.CustomHttp409
 import com.goliath.emojihub.springboot.domain.user.dao.UserDao
-import com.goliath.emojihub.springboot.domain.user.dto.LoginRequest
-import com.goliath.emojihub.springboot.domain.user.dto.SignUpRequest
 import com.goliath.emojihub.springboot.domain.user.dto.UserDto
 import com.goliath.emojihub.springboot.global.auth.JwtTokenProvider
 import com.goliath.emojihub.springboot.global.exception.CustomHttp403
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
-interface UserService {
-    fun getUsers(): List<UserDto>
-    fun signUp(signUpRequest: SignUpRequest): UserDto.AuthToken
-    fun login(loginRequest: LoginRequest): UserDto.AuthToken
-    fun logout()
-    fun signOut(username: String)
-}
-
 @Service
-class UserServiceImpl(
+class UserService(
     private val userDao: UserDao,
     private val jwtTokenProvider: JwtTokenProvider,
     private val passwordEncoder: PasswordEncoder,
     private val emojiDao: EmojiDao,
     private val postDao: PostDao,
-    ) : UserService {
-    override fun getUsers(): List<UserDto> {
+) {
+    fun getUsers(): List<UserDto> {
         return userDao.getUsers()
     }
 
-    override fun signUp(signUpRequest: SignUpRequest): UserDto.AuthToken {
-        if (userDao.existUser(signUpRequest.username)) {
+    fun signUp(email: String, username: String, password: String): UserDto.AuthToken {
+        if (userDao.existUser(username)) {
             throw CustomHttp409("Id already exists.")
         }
-        signUpRequest.password = passwordEncoder.encode(signUpRequest.password)
-        userDao.insertUser(signUpRequest)
-        val authToken = jwtTokenProvider.createToken(signUpRequest.username)
+        val encodedPassword = passwordEncoder.encode(password)
+        val user = UserDto(
+            email = email,
+            username = username,
+            password = encodedPassword
+        )
+        userDao.insertUser(user)
+        val authToken = jwtTokenProvider.createToken(username)
         return UserDto.AuthToken(authToken)
     }
 
-    override fun login(loginRequest: LoginRequest): UserDto.AuthToken {
-        val user = userDao.getUser(loginRequest.username) ?: throw CustomHttp404("Id doesn't exist.")
-        if (!passwordEncoder.matches(loginRequest.password, user.password)) {
+    fun login(username: String, password: String): UserDto.AuthToken {
+        val user = userDao.getUser(username) ?: throw CustomHttp404("Id doesn't exist.")
+        if (!passwordEncoder.matches(password, user.password)) {
             throw CustomHttp401("Password is incorrect.")
         }
         val authToken = jwtTokenProvider.createToken(user.username)
         return UserDto.AuthToken(authToken)
     }
 
-    override fun logout() {
+    fun logout() {
         return
     }
 
-    override fun signOut(username: String) {
+    fun signOut(username: String) {
         val user = userDao.getUser(username) ?: throw CustomHttp404("User doesn't exist.")
         val emojiIds = user.created_emojis
         val postIds = user.created_posts
