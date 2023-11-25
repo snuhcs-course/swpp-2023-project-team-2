842e3dea74464959dae48e1ca828777dab235e0d
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2023-11-25 17:31:18 +0900

    feat: modifications on unit test scripts for use case classes

--- android/app/src/test/java/com/goliath/emojihub/usecases/PostUseCaseImplTest.kt
+++ android/app/src/test/java/com/goliath/emojihub/usecases/PostUseCaseImplTest.kt
@@ -12,6 +12,7 @@ import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
 import io.mockk.spyk
+import io.mockk.verify
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
@@ -25,7 +26,7 @@ import retrofit2.Response
 @RunWith(JUnit4::class)
 class PostUseCaseImplTest {
     private val postRepository = mockk<PostRepository>()
-    private val apiErrorController = mockk<ApiErrorController>()
+    private val apiErrorController = spyk<ApiErrorController>()
     private val postUseCaseImpl = PostUseCaseImpl(postRepository, apiErrorController)
 
     @Test
@@ -33,9 +34,7 @@ class PostUseCaseImplTest {
         // given
         val samplePagingPostData = mockk<PagingData<Post>>()
         // when
-        runBlocking {
-            postUseCaseImpl.updatePostList(samplePagingPostData)
-        }
+        runBlocking { postUseCaseImpl.updatePostList(samplePagingPostData) }
         // then
         assertEquals(samplePagingPostData, postUseCaseImpl.postList.value)
     }
@@ -49,9 +48,7 @@ class PostUseCaseImplTest {
             postRepository.fetchPostList()
         } returns samplePostPagingDataFlow
         // when
-        val fetchedPostPagingDataFlow = runBlocking {
-            postUseCaseImpl.fetchPostList()
-        }
+        val fetchedPostPagingDataFlow = runBlocking { postUseCaseImpl.fetchPostList() }
         // then
         coVerify(exactly = 1) { postRepository.fetchPostList() }
         runBlocking {
@@ -70,28 +67,91 @@ class PostUseCaseImplTest {
             postRepository.uploadPost(any())
         } returns Response.success(mockk())
         // when
-        val isSuccess = runBlocking {
-            postUseCaseImpl.uploadPost(sampleContent)
-        }
+        val isSuccess = runBlocking { postUseCaseImpl.uploadPost(sampleContent) }
         // then
-        coVerify(exactly = 1) {
-            postRepository.uploadPost(UploadPostDto(sampleContent))
-        }
+        coVerify(exactly = 1) { postRepository.uploadPost(UploadPostDto(sampleContent)) }
         assertTrue(isSuccess)
     }
 
-    // @Test
-    // No return value
-    fun getPostWithId() {
+    @Test
+    fun uploadPost_failure_returnsFalse() {
+        // given
+        val sampleContent = "sample content"
+        val sampleErrorCode = 403
+        coEvery {
+            postRepository.uploadPost(any())
+        } returns Response.error(sampleErrorCode, mockk(relaxed = true))
+        // when
+        val isSuccess = runBlocking { postUseCaseImpl.uploadPost(sampleContent) }
+        // then
+        coVerify(exactly = 1) { postRepository.uploadPost(UploadPostDto(sampleContent)) }
+        verify(exactly = 1) { apiErrorController.setErrorState(sampleErrorCode) }
+        assertFalse(isSuccess)
     }
 
-    // @Test
-    // No return value
-    fun editPost() {
+     @Test
+    fun getPostWithId_success_returnsPostDto() {
+        // given
+        val sampleId = "sampleId"
+        val samplePostDto = PostDto(
+            sampleId,
+            "sampleCreatedAt",
+            "sampleUpdatedAt",
+            "sampleUserId",
+            "sampleUserAvatarUrl",
+        )
+        coEvery {
+            postRepository.getPostWithId(any())
+        } returns samplePostDto
+        // when
+        val postDto = runBlocking { postUseCaseImpl.getPostWithId(sampleId) }
+        // then
+        coVerify(exactly = 1) { postRepository.getPostWithId(sampleId) }
+        assertEquals(samplePostDto, postDto)
     }
 
-    // @Test
+    @Test
+    fun getPostWithId_failure_returnsNull() {
+        // given
+        val sampleId = "sampleId"
+        coEvery {
+            postRepository.getPostWithId(any())
+        } returns null
+        // when
+        val postDto = runBlocking { postUseCaseImpl.getPostWithId(sampleId) }
+        // then
+        coVerify(exactly = 1) { postRepository.getPostWithId(sampleId) }
+        assertNull(postDto)
+    }
+
+     @Test
     // No return value
-    fun deletePost() {
+    // TODO: NOT FULLY IMPLEMENTED
+    fun editPost_verifyRunExactlyOnce() {
+        // given
+        val sampleId = "sampleId"
+        val sampleContent = "sampleContent"
+        coEvery {
+            postRepository.editPost(any(), any())
+        } returns Unit
+        // when
+        runBlocking { postUseCaseImpl.editPost(sampleId, sampleContent) }
+        // then
+        coVerify(exactly = 1) { postRepository.editPost(sampleId, sampleContent) }
+    }
+
+     @Test
+    // No return value
+    // TODO: NOT FULLY IMPLEMENTED
+    fun deletePost_verifyRunExactlyOnce() {
+        // given
+        val sampleId = "sampleId"
+        coEvery {
+            postRepository.deletePost(any())
+        } returns Unit
+        // when
+        runBlocking { postUseCaseImpl.deletePost(sampleId) }
+        // then
+        coVerify(exactly = 1) { postRepository.deletePost(sampleId) }
     }
 }
--- android/app/src/test/java/com/goliath/emojihub/usecases/UserUseCaseImplTest.kt
+++ android/app/src/test/java/com/goliath/emojihub/usecases/UserUseCaseImplTest.kt
@@ -1,13 +1,16 @@
 package com.goliath.emojihub.usecases
 
+import android.app.Application
 import com.goliath.emojihub.EmojiHubApplication
 import com.goliath.emojihub.data_sources.ApiErrorController
 import com.goliath.emojihub.data_sources.SharedLocalStorage
 import com.goliath.emojihub.mockLogClass
+import com.goliath.emojihub.models.RegisterUserDto
 import com.goliath.emojihub.models.User
 import com.goliath.emojihub.models.UserDto
 import com.goliath.emojihub.models.responses.LoginResponseDto
 import com.goliath.emojihub.repositories.remote.UserRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
@@ -36,11 +39,56 @@ class UserUseCaseImplTest {
     }
 
     @Test
-    fun registerUser() {
+    fun registerUser_withValidUserInfo_returnsTrue() {
+        // given
+        val sampleRegisterUserDto = RegisterUserDto(
+            "sampleEmail",
+            "sampleName",
+            "samplePassword"
+        )
+        val sampleAccessToken = "sampleAccessToken"
+        coEvery {
+            userRepository.registerUser(any())
+        } returns Response.success(LoginResponseDto(sampleAccessToken))
+        // when
+        val isSuccessfulRegister = runBlocking {
+            userUseCaseImpl.registerUser(
+                sampleRegisterUserDto.email,
+                sampleRegisterUserDto.name,
+                sampleRegisterUserDto.password
+            )
+        }
+        // then
+        coVerify { userRepository.registerUser(any()) }
+        assertTrue(isSuccessfulRegister)
     }
 
     @Test
-    fun login_validUserInfo_updateAccessToken() {
+    fun registerUser_withInvalidUserInfo_returnsFalse() {
+        // given
+        val sampleRegisterUserDto = RegisterUserDto(
+            "sampleEmail",
+            "sampleName",
+            "samplePassword"
+        )
+        coEvery {
+            userRepository.registerUser(any())
+        } returns Response.error(400, mockk(relaxed=true))
+        // when
+        val isSuccessfulRegister = runBlocking {
+            userUseCaseImpl.registerUser(
+                sampleRegisterUserDto.email,
+                sampleRegisterUserDto.name,
+                sampleRegisterUserDto.password
+            )
+        }
+        // then
+        coVerify { userRepository.registerUser(any()) }
+        assertFalse(isSuccessfulRegister)
+    }
+
+    @Test
+    fun login_withValidUserInfo_updateAccessToken() {
         // given
         val sampleName = "sampleName"
         val samplePassword = "samplePassword"
@@ -48,18 +96,8 @@ class UserUseCaseImplTest {
         coEvery {
             userRepository.login(any())
         } returns Response.success(LoginResponseDto(sampleAccessToken))
-        mockkStatic(EmojiHubApplication::class)
-        val mockPreferences = mockk<SharedLocalStorage>()
-        every {
-            EmojiHubApplication.preferences
-        } returns mockPreferences
-        every {
-            mockPreferences.accessToken = any()
-        } returns Unit
         // when
-        runBlocking {
-            userUseCaseImpl.login(sampleName, samplePassword)
-        }
+        runBlocking { userUseCaseImpl.login(sampleName, samplePassword) }
         // then
         coVerify { userRepository.login(any()) }
         assertEquals(
@@ -68,11 +106,13 @@ class UserUseCaseImplTest {
         )
     }
 
-    @Test
+    // @Test
+    // TODO: Not yet implemented
     fun logout() {
     }
 
-    @Test
+    // @Test
+    // TODO: Not yet implemented
     fun signOut() {
     }
 }
