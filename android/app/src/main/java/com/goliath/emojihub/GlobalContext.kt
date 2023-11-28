dc21f81a2ddf183896236af497a4fa2fd0967875
peng-u-0807 <penguin0807@snu.ac.kr>
2023-11-29 01:18:05 +0900

    refactor: refactor project to use single `NavHost`

--- android/app/src/main/java/com/goliath/emojihub/GlobalContext.kt
+++ android/app/src/main/java/com/goliath/emojihub/GlobalContext.kt
@@ -2,7 +2,12 @@ package com.goliath.emojihub
 
 import androidx.compose.runtime.compositionLocalOf
 import androidx.navigation.NavController
+import com.goliath.emojihub.data_sources.BottomNavigationController
 
 val LocalNavController = compositionLocalOf<NavController> {
+    throw RuntimeException("")
+}
+
+val LocalBottomNavigationController = compositionLocalOf<BottomNavigationController> {
     throw RuntimeException("")
 }
