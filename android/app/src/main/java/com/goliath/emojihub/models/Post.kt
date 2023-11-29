daf63003723e50c49264279310882ac6c2b78300
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2023-11-30 00:59:21 +0900

    test: apply testing on fetchMyPostList with fake PostList

--- android/app/src/main/java/com/goliath/emojihub/models/Post.kt
+++ android/app/src/main/java/com/goliath/emojihub/models/Post.kt
@@ -10,8 +10,32 @@ class Post(
     val modifiedAt: String = dto.modifiedAt
     val createdBy: String = dto.createdBy
     val content: String = dto.content
-    val reaction: List<String> = listOf()
-    //val reaction: List<String> = dto.reaction
+    val reaction: List<String> = dto.reaction
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Post
+        if (id != other.id) return false
+        if (createdAt != other.createdAt) return false
+        if (modifiedAt != other.modifiedAt) return false
+        if (createdBy != other.createdBy) return false
+        if (content != other.content) return false
+        if (reaction != other.reaction) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + createdAt.hashCode()
+        result = 31 * result + modifiedAt.hashCode()
+        result = 31 * result + createdBy.hashCode()
+        result = 31 * result + content.hashCode()
+        result = 31 * result + reaction.hashCode()
+        return result
+    }
 }
 
 data class PostDto(
@@ -20,7 +44,7 @@ data class PostDto(
     @SerializedName("modified_at") val modifiedAt: String,
     @SerializedName("created_by") val createdBy: String,
     val content: String,
-    //@SerializedName("reaction") val reaction: List<String>
+    @SerializedName("reaction") val reaction: List<String>
 )
 
 data class UploadPostDto(
@@ -35,7 +59,7 @@ val dummyPost = Post(
         content = "조금 전에 앞에 계신 분이 실수로 지갑을 흘리셨다. " +
                 "지갑이 하수구 구멍으로 빠지려는 찰나, 발로 굴러가는 지갑을 막아서 다행히 참사는 막을 수 있었다. " +
                 "지갑 주인분께서 감사하다고 카페 드림에서 커피도 한 잔 사주셨다.",
-        modifiedAt = "2023.10.23"
-        //reaction = listOf("good", "check", "good")
+        modifiedAt = "2023.10.23",
+        reaction = listOf("good", "check", "good")
     )
 )
--- android/app/src/test/java/com/goliath/emojihub/repositories/remote/PostRepositoryImplTest.kt
+++ android/app/src/test/java/com/goliath/emojihub/repositories/remote/PostRepositoryImplTest.kt
@@ -3,8 +3,8 @@ package com.goliath.emojihub.repositories.remote
 import androidx.paging.testing.asSnapshot
 import com.goliath.emojihub.data_sources.api.PostApi
 import com.goliath.emojihub.mockLogClass
-import com.goliath.emojihub.models.PostDto
 import com.goliath.emojihub.models.UploadPostDto
+import com.goliath.emojihub.samplePostDto
 import io.mockk.coEvery
 import io.mockk.coVerify
 import retrofit2.Response
@@ -20,16 +20,7 @@ import org.junit.runners.JUnit4
 class PostRepositoryImplTest {
     private val postApi = mockk<PostApi>()
     private val postRepositoryImpl = PostRepositoryImpl(postApi)
-    private val samplePostDto = PostDto(
-        id = "1234",
-        createdAt = "2023.09.16",
-        createdBy = "channn",
-        content = "조금 전에 앞에 계신 분이 실수로 지갑을 흘리셨다. " +
-                "지갑이 하수구 구멍으로 빠지려는 찰나, 발로 굴러가는 지갑을 막아서 다행히 참사는 막을 수 있었다. " +
-                "지갑 주인분께서 감사하다고 카페 드림에서 커피도 한 잔 사주셨다.",
-        modifiedAt = "2023.10.23"
-        //reaction = listOf("good", "check", "good")
-    )
+
     @Before
     fun setUp() {
         mockLogClass()
@@ -50,6 +41,27 @@ class PostRepositoryImplTest {
         val fetchedPostDtoList = runBlocking { fetchedPostPagingDataFlow.asSnapshot() }
         // then
         coVerify(exactly = 2) { postApi.fetchPostList(any()) }
+        runBlocking {
+            assertEquals(expectedFetchedPostDtoList.size, fetchedPostDtoList.size)
+            assertEquals(expectedFetchedPostDtoList, fetchedPostDtoList)
+        }
+    }
+
+    @Test
+    fun fetchMyPostList_returnsFlowOfPagingDataOfPostDto() {
+        // given
+        val numSamplePosts = 10
+        val samplePostDtoList = List(numSamplePosts) { samplePostDto }
+        val expectedFetchedPostDtoList = List(numSamplePosts*2) { samplePostDto }
+        // *2 because of .asSnapshot() load one more time
+        coEvery {
+            postApi.fetchMyPostList(any())
+        } returns Response.success(samplePostDtoList)
+        // when
+        val fetchedPostPagingDataFlow = runBlocking { postRepositoryImpl.fetchMyPostList() }
+        val fetchedPostDtoList = runBlocking { fetchedPostPagingDataFlow.asSnapshot() }
+        // then
+        coVerify(exactly = 2) { postApi.fetchMyPostList(any()) }
         runBlocking {
             assertEquals(expectedFetchedPostDtoList.size, fetchedPostDtoList.size)
             assertEquals(expectedFetchedPostDtoList, fetchedPostDtoList)
@@ -118,7 +130,7 @@ class PostRepositoryImplTest {
             postApi.editPost(any(), any())
         } returns Response.success(Unit)
         // when
-        val response = runBlocking {
+        runBlocking {
             postRepositoryImpl.editPost(samplePostDto.id, samplePostDto.content)
         }
         // then
@@ -137,7 +149,7 @@ class PostRepositoryImplTest {
             postApi.deletePost(any())
         } returns Response.success(Unit)
         // when
-        val response = runBlocking { postRepositoryImpl.deletePost(samplePostDto.id) }
+        runBlocking { postRepositoryImpl.deletePost(samplePostDto.id) }
         // then
         coVerify(exactly = 1) { postApi.deletePost(samplePostDto.id) }
     }
