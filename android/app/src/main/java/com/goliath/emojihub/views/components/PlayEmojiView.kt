274bab09dd58ae11de5d3a5c75848d85cd5bfc55
2018JunYoungLim <icarus001104@gmail.com>
2023-12-01 01:18:43 +0900

    fix: video playback issue when navigating away from the view

--- android/app/src/main/java/com/goliath/emojihub/views/components/PlayEmojiView.kt
+++ android/app/src/main/java/com/goliath/emojihub/views/components/PlayEmojiView.kt
@@ -16,6 +16,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.FileDownload
 import androidx.compose.material.icons.filled.FileDownloadOff
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -58,6 +59,13 @@ fun PlayEmojiView(
             setMediaItem(MediaItem.fromUri(currentEmoji.videoLink))
             repeatMode = Player.REPEAT_MODE_ALL
             prepare()
+        }
+    }
+
+    DisposableEffect(Unit) {
+        onDispose {
+            exoPlayer.stop()
+            exoPlayer.release()
         }
     }
 
