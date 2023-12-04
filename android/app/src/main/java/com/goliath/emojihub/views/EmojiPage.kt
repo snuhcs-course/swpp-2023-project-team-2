7a6e76b8742e4d7757cc411956b299aa47a5220e
peng-u-0807 <penguin0807@snu.ac.kr>
2023-12-05 00:39:35 +0900

    feat: disable `LoginButton` if username or password isEmpty

--- android/app/src/main/java/com/goliath/emojihub/views/EmojiPage.kt
+++ android/app/src/main/java/com/goliath/emojihub/views/EmojiPage.kt
@@ -1,37 +1,35 @@
 package com.goliath.emojihub.views
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import android.Manifest
 import android.content.pm.PackageManager
-import android.os.Build
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import com.goliath.emojihub.views.components.EmojiCell
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -42,16 +40,17 @@ import com.goliath.emojihub.ui.theme.Color.Black
 import com.goliath.emojihub.ui.theme.Color.LightGray
 import com.goliath.emojihub.ui.theme.Color.White
 import com.goliath.emojihub.viewmodels.EmojiViewModel
+import com.goliath.emojihub.views.components.EmojiCell
 import com.goliath.emojihub.views.components.EmojiCellDisplay
 import com.goliath.emojihub.views.components.TopNavigationBar
 
 @Composable
-fun EmojiPage(
-) {
+fun EmojiPage() {
     val context = LocalContext.current
     val navController = LocalNavController.current
 
     val viewModel = hiltViewModel<EmojiViewModel>()
+    val emojiList = viewModel.emojiList.collectAsLazyPagingItems()
 
     val permissionLauncher = rememberLauncherForActivityResult(
         ActivityResultContracts.RequestPermission()
@@ -66,8 +65,6 @@ fun EmojiPage(
             navController.navigate(NavigationDestination.TransformVideo)
         }
     }
-
-    val emojiList = viewModel.emojiList.collectAsLazyPagingItems()
 
     LaunchedEffect(Unit) {
         viewModel.fetchEmojiList()
