6babf482f09f1ae2ad4267c8abb7d41ee048c5fc
Dawit Fikru Amanu <bfiqiruu@gmail.com>
2023-12-05 01:13:55 +0900

    update postDto's reaction variable to match backend changes

--- android/app/src/main/java/com/goliath/emojihub/models/Post.kt
+++ android/app/src/main/java/com/goliath/emojihub/models/Post.kt
@@ -10,7 +10,7 @@ class Post(
     val modifiedAt: String = dto.modifiedAt
     val createdBy: String = dto.createdBy
     val content: String = dto.content
-    val reaction: List<String> = dto.reaction
+    val reaction: List<ReactionWithEmojiUnicode> = dto.reaction
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -44,22 +44,14 @@ data class PostDto(
     @SerializedName("modified_at") val modifiedAt: String,
     @SerializedName("created_by") val createdBy: String,
     val content: String,
-    @SerializedName("reactions") val reaction: List<String>
+    @SerializedName("reactions") val reaction: List<ReactionWithEmojiUnicode>
 )
 
 data class UploadPostDto(
     @SerializedName("content") val content: String
 )
 
-val dummyPost = Post(
-    PostDto(
-        id = "1234",
-        createdAt = "2023.09.16",
-        createdBy = "channn",
-        content = "조금 전에 앞에 계신 분이 실수로 지갑을 흘리셨다. " +
-                "지갑이 하수구 구멍으로 빠지려는 찰나, 발로 굴러가는 지갑을 막아서 다행히 참사는 막을 수 있었다. " +
-                "지갑 주인분께서 감사하다고 카페 드림에서 커피도 한 잔 사주셨다.",
-        modifiedAt = "2023.10.23",
-        reaction = listOf("good", "check", "good")
-    )
+data class ReactionWithEmojiUnicode(
+    var id: String = "",
+    var emoji_unicode: String = ""
 )
