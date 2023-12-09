b26447823b71da0421b1563630a8e913a12745c7
peng-u-0807 <penguin0807@snu.ac.kr>
2023-12-09 21:57:49 +0900

    fix: change `CustomBottomSheet` button UI

--- android/app/src/main/java/com/goliath/emojihub/views/components/CustomBottomSheet.kt
+++ android/app/src/main/java/com/goliath/emojihub/views/components/CustomBottomSheet.kt
@@ -3,17 +3,11 @@ package com.goliath.emojihub.views.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Text
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
@@ -26,8 +20,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
@@ -37,7 +29,6 @@ import com.goliath.emojihub.NavigationDestination
 import com.goliath.emojihub.extensions.toEmoji
 import com.goliath.emojihub.models.Emoji
 import com.goliath.emojihub.ui.theme.Color.EmojiHubDividerColor
-import com.goliath.emojihub.ui.theme.Color.LightGray
 import com.goliath.emojihub.ui.theme.Color.White
 import com.goliath.emojihub.viewmodels.EmojiViewModel
 import com.goliath.emojihub.viewmodels.PostViewModel
@@ -104,7 +95,7 @@ fun CustomBottomSheet (
                         EmojiClassFilterRow(
                             emojiClass = emojiUnicodeFilters,
                             emojiCounts = emojiCounts,
-                            onEmojiClassSelected = { selectedEmojiClass = it}
+                            onEmojiClassSelected = { selectedEmojiClass = it }
                         ) {
                             items(emojiUnicodeFilters.size) { unicode ->
                                 EmojiClassFilterButton(
@@ -125,54 +116,27 @@ fun CustomBottomSheet (
                 Column(Modifier.padding(horizontal = 16.dp)) {
                     Row(
                         modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
+                        horizontalArrangement = Arrangement.Start,
                         verticalAlignment = Alignment.CenterVertically
                     ) {
-                        OutlinedButton(
-                            onClick = {
-                                displayMyCreatedEmojis = true
-                            },
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(end = 8.dp),
-                            shape = RoundedCornerShape(50.dp),
-                            colors = ButtonDefaults.buttonColors(
-                                backgroundColor = if (displayMyCreatedEmojis) LightGray else White,
-                                contentColor = Color.Black
-                            )
+                        EmojiClassFilterButton(
+                            text = "내가 만든 이모지",
+                            isSelected = displayMyCreatedEmojis
                         ) {
-                            Text(
-                                text = "내가 만든 이모지",
-                                fontWeight = FontWeight.Bold
-                            )
+                            displayMyCreatedEmojis = true
                         }
 
-                        OutlinedButton(
-                            onClick = {
-                                displayMyCreatedEmojis = false
-                            },
-                            modifier = Modifier
-                                .weight(1f)
-                                .padding(start = 8.dp),
-                            shape = RoundedCornerShape(50.dp),
-                            colors = ButtonDefaults.buttonColors(
-                                backgroundColor = if (!displayMyCreatedEmojis) LightGray else White,
-                                contentColor = Color.Black
-                            )
+                        EmojiClassFilterButton(
+                            text = "저장된 이모지",
+                            isSelected = !displayMyCreatedEmojis
                         ) {
-                            Text(
-                                text = "저장된 이모지",
-                                fontWeight = FontWeight.Bold
-                            )
+                            displayMyCreatedEmojis = false
                         }
                     }
                 }
             }
         }
-
         Divider(color = EmojiHubDividerColor, thickness = 0.5.dp)
-        Spacer(modifier = Modifier.height(16.dp))
-
         Column(Modifier.padding(horizontal = 16.dp)) {
             LazyVerticalGrid(
                 columns = GridCells.Fixed(2),
@@ -182,7 +146,6 @@ fun CustomBottomSheet (
             ) {
                 when (bottomSheetContent) {
                     BottomSheetContent.EMPTY -> {}
-
                     BottomSheetContent.VIEW_REACTION -> {
                         items(reactionList.itemCount) { index ->
                             reactionList[index]?.let {
