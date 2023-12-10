f04c23d05e2c2d9d3f1ac892d53c3eb6f1c887d8
Wooyeol Lee <wooyeol0519@snu.ac.kr>
2023-12-10 19:37:42 +0900

    test: implement ui display test on LoginPage

--- android/app/src/androidTest/java/com/goliath/emojihub/integration_tests/LoginPageTest.kt
+++ android/app/src/androidTest/java/com/goliath/emojihub/integration_tests/LoginPageTest.kt
@@ -1,6 +1,7 @@
 package com.goliath.emojihub.integration_tests
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertHasNoClickAction
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.hasText
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
@@ -8,9 +9,7 @@ import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.performTextInput
-import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.goliath.emojihub.RootActivity
@@ -20,6 +19,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
+@OptIn(ExperimentalTestApi::class)
 class LoginPageTest {
 
     @get:Rule
@@ -28,84 +28,93 @@ class LoginPageTest {
 
     @Test
     fun usernameField_IsDisplayed() {
-        composeTestRule.onNodeWithTag("UsernameField")
+        composeTestRule.onNodeWithText("Username")
             .assertIsDisplayed()
     }
 
     @Test
     fun passwordField_IsDisplayed() {
-        composeTestRule.onNodeWithTag("PasswordField")
+        composeTestRule.onNodeWithText("Password")
             .assertIsDisplayed()
     }
 
     @Test
     fun loginButton_IsDisplayed() {
-        composeTestRule.onNodeWithTag("LoginButton")
+        composeTestRule.onNodeWithText("로그인")
             .assertIsDisplayed()
     }
 
     @Test
     fun registerButton_IsDisplayed() {
-        composeTestRule.onNodeWithTag("RegisterButton")
+        composeTestRule.onNodeWithText("계정 생성")
             .assertIsDisplayed()
     }
 
     @Test
     fun guestModeButton_IsDisplayed() {
-        composeTestRule.onNodeWithTag("GuestModeButton")
+        composeTestRule.onNodeWithText("비회원 모드로 시작하기")
             .assertIsDisplayed()
     }
 
 
     fun loginButton_Clicked_WithEmptyFields_ShowsError() {
-        TODO("NOT IMPLEMENTED YET")
-        composeTestRule.onNodeWithTag("LoginButton")
-            .performClick()
-
-        // Assuming an error message is shown when fields are empty
-        onView(withText("Please fill in all fields"))
-            .check(matches(isDisplayed()))
+        composeTestRule.onNodeWithText("로그인")
+            .assertHasNoClickAction()
     }
 
     @Test
     fun loginButton_Clicked_WithInvalidUsername_ShowsNotFoundError(){
         // If username is invalid, password input doesn't matter
-        composeTestRule.onNodeWithTag("UsernameField")
+        composeTestRule.onNodeWithText("Username")
             .performTextInput("invalidUsername")
-        composeTestRule.onNodeWithTag("LoginButton")
+        composeTestRule.onNodeWithText("Password")
+            .performTextInput("password7")
+        composeTestRule.onNodeWithText("로그인")
             .performClick()
         // Wait until the error message is shown
-        composeTestRule.waitForIdle()
+        composeTestRule.waitUntilExactlyOneExists(hasText("에러"))
         // Assuming an error message is shown when username is invalid
         composeTestRule.onNodeWithText(CustomError.NOT_FOUND.body(), useUnmergedTree = true)
             .assertIsDisplayed()
+        // reset the error message
+        composeTestRule.onNodeWithText("확인")
+            .performClick()
     }
 
     @Test
     fun loginButton_Clicked_WithInvalidPassword_ShowsUnauthorizedError(){
-        composeTestRule.onNodeWithTag("UsernameField")
+        composeTestRule.onNodeWithText("Username")
             .performTextInput("username7")
-        composeTestRule.onNodeWithTag("PasswordField")
+        composeTestRule.onNodeWithText("Password")
             .performTextInput("invalidPassword")
-        composeTestRule.onNodeWithTag("LoginButton")
+        composeTestRule.onNodeWithText("로그인")
             .performClick()
         // Wait until the error message is shown
         // FIXME: this wait command is not stable, not actually it meant to be
-        // another wait command may be needed
-        composeTestRule.waitForIdle()
+        // FIXME: another wait command may be needed
+        composeTestRule.waitUntilExactlyOneExists(hasText("에러"))
         // Assuming an error message is shown when password is invalid
-        composeTestRule.onNodeWithText(CustomError.UNAUTHORIZED.body(), useUnmergedTree = true)
+        composeTestRule.onNodeWithText(CustomError.UNAUTHORIZED.body(),
+            useUnmergedTree = true, substring = true, ignoreCase = true)
             .assertIsDisplayed()
+//        // reset error state
+//        composeTestRule.onNodeWithText("확인")
+//            .performClick()
+//        composeTestRule.waitForIdle()
+//        composeTestRule.onNodeWithText("Username")
+//            .performTextClearance()
+//        composeTestRule.onNodeWithText("Password")
+//            .performTextClearance()
     }
 
-    @OptIn(ExperimentalTestApi::class)
-//    @Test
+    // @Test
+    // FIXME: RootActivity should be reset after each test
     fun loginButton_Clicked_WithValidCredentials_ShowsFeedPage(){
-        composeTestRule.onNodeWithTag("UsernameField")
+        composeTestRule.onNodeWithText("Username")
             .performTextInput("username7")
-        composeTestRule.onNodeWithTag("PasswordField")
+        composeTestRule.onNodeWithText("Password")
             .performTextInput("password7")
-        composeTestRule.onNodeWithTag("LoginButton")
+        composeTestRule.onNodeWithText("로그인")
             .performClick()
         // Wait until the feed page is shown
         // FIXME: this wait command is not stable, not actually it meant to be
@@ -113,30 +122,33 @@ class LoginPageTest {
         // Assuming the homepage is shown when credentials are valid
         composeTestRule.onNodeWithText("Feed")
             .assertIsDisplayed()
+        // reset the authentication
+        composeTestRule.activity.applicationContext.getSharedPreferences("EMOJI_HUB", 0)
+            .edit().putString("accessToken", null).apply()
+        composeTestRule.waitUntilExactlyOneExists(hasText("로그인"))
     }
 
-    @OptIn(ExperimentalTestApi::class)
-    @Test
+//    @Test
     fun registerButton_Clicked_ShowsSignUpPage(){
         composeTestRule.onNodeWithTag("RegisterButton")
             .performClick()
         // Wait until the sign up page is shown
         // FIXME: this wait command is not stable, not actually it meant to be
-        composeTestRule.waitUntilAtLeastOneExists(hasText("계정 생성"))
+        composeTestRule.waitUntilExactlyOneExists(hasText("계정 생성"))
         // Assuming the sign up page is shown when register button is clicked
         composeTestRule.onNodeWithText("계정 생성")
             .assertIsDisplayed()
     }
 
-    // @Test
+//     @Test
     // TODO: Not implemented yet
     fun guestModeButton_Clicked_ShowsFeedPage(){
-        composeTestRule.onNodeWithTag("GuestModeButton")
+        composeTestRule.onNodeWithText("비회원")
             .performClick()
         // Wait until the feed page is shown
-        composeTestRule.waitForIdle()
+        composeTestRule.waitUntilAtLeastOneExists(hasText("Feed"))
         // Assuming the homepage is shown when guest mode button is clicked
-        composeTestRule.onNodeWithTag("FeedPage")
+        composeTestRule.onNodeWithText("Feed")
             .assertIsDisplayed()
     }
 }
